ab8806b1c552bdb0dabd54380abc716d4444e635
enemarke <enemarke@gmail.com>
2016-10-29 07:54:29 +0200

    create user should be working, still missing spec test

--- lib/puppet/provider/bmc_user/ipmitool.rb
+++ lib/puppet/provider/bmc_user/ipmitool.rb
@@ -25,27 +25,50 @@ Puppet::Type.type(:bmc_user).provide(:ipmitool) do
     user
   end
 
+  def get_current_value(var)
+  user=get_user_info
+  debug "current proto: #{user[var]}"
+  user[var]
+  end
 
   def exits?
     user = get_user_info
     user[resource[:name]]
   end
 
-  def destroy
-    #We can't delete at user, so it is disabled and renamed to xxxxxx. Its renamed because otherwise the exits?
-    #would resturn true. Is there a way to detect if user is enabled or disabled? Maybe with a RAW command?
-    ipmitool('user', 'disable', user[resource[:name]['id']])
-    ipmitool('user', 'set', 'name', user[resource[:name]]['id'], 'xxxxxx')
+  def password
+  get_current_value('password')
   end
 
-  def create
-    #Enabling the user in case it was diabled.
-    ipmitool('user', 'enable', resource[:userid])
-    ipmitool('user', 'set', 'name', resource[:userid],resource[:name] )
-    #Running as exec so password isn't logged during puppet debug run.
-    unless system( "ipmitool user set password #{resurce[:userid]} #{resource[:password]}" )
+  def pasword=(value)
+    unless system( "ipmitool user set password #{resurce[:userid]} #{value}" )
       raise Puppet::Error, "Failed to set password for #{resouce[:name]}"
     end
-    ipmitool('channel', 'setaccess', resource[:channel], resource[:userid], "privilege=#{resource[:privilege]}")
+    debug "Setting password for user '#{resurce[:userid]}' using comand 'ipmitool user set password #{resurce[:userid]} XXXX'"
+  end
+
+  def enable
+    #TODO need to do some magic here, inorder to detect if user is enabled or disaled
+    false
+  end
+
+  def enable=(value)
+    ipmitool('user', 'enable', value)
+  end
+
+  def user
+    get_current_value('user')
+  end
+
+  def user=(value)
+    ipmitool('user', 'set', 'name', resource[:userid],value )
+  end
+
+  def priviliege
+    get_current_value('privilege')
+  end
+
+  def user=(value)
+    ipmitool('channel', 'setaccess', resource[:channel], resource[:userid], "privilege=#{value}")
   end
 end
