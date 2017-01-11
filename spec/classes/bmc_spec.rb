c1a0e1cc3fc9d1bedb92008dd7e1b7c86f62946b
enemarke <enemarke@gmail.com>
2017-01-11 20:51:17 +0100

    Added some more class spec test.

--- spec/classes/bmc_spec.rb
+++ spec/classes/bmc_spec.rb
@@ -1,40 +1,44 @@
 #!/usr/bin/env rspec
-#
 require 'spec_helper'
 
 describe "bmc", :type => :class do
-  context "on debian it is expected to compile" do
-  let(:facts) { {
-      :osfamily => 'Debian'
-  } }
-  it { is_expected.to compile.with_all_deps }
-  it { is_expected.to contain_package('ipmitool').with(
-      {
-          :ensure => :present,
-      }
-  ) }
+  let :facts do
+    {
+        osfamily: osfamily,
+        operatingsystemmajrelease: operatingsystemmajrelease,
+        :manufactor_id => '674',
+        :lsbdistid => '14.04',
+        :lsbdistcodename => 'trusty'
+    }
   end
-  context "On a RedHat it is expected to compile" do
-    let :facts do
-      {
-          :osfamily => 'RedHat',
-          :operatingsystemmajrelease => 5
-      }
+  let(:osfamily) { 'Debian' }
+  let(:operatingsystemmajrelease) { 5 }
+
+  context "on debian it is expected to compile" do
+    it { is_expected.to compile.with_all_deps }
+    it { is_expected.to contain_package('ipmitool').with( {:ensure => :present} ) }
+  end
+  context "On RedHat it is expected to compile" do
+    let(:osfamily) { 'Redhat' }
+    it { is_expected.to compile.with_all_deps }
+
+    context "On Redhat 5 it should contain OpenIPMI and OpenIPMI-tools" do
+      it { is_expected.to contain_package('OpenIPMI').with( {:ensure => :present} ) }
+      it { is_expected.to contain_package('OpenIPMI-tools').with( {:ensure => :present} ) }
     end
 
-    it { is_expected.to compile.with_all_deps }
-    it { is_expected.to contain_package('OpenIPMI').with(
-        {
-            :ensure => :present,
-        }
-    ) }
-    it { is_expected.to contain_package('OpenIPMI-tools').with(
-        {
-            :ensure => :present,
-        }
-    ) }
+    context "On Redhat 6 and 7 it should contain ipmitool" do
+      [6, 7].each do |operatingsystemrelease|
+        let(:operatingsystemmajrelease) { operatingsystemrelease }
+        it { is_expected.to contain_package('ipmitool').with( {:ensure => :present} ) }
+      end
+    end
   end
 
+  context "On Dell servcers it should install racadm" do
+    let(:params) { {:manage_repo => true} }
+    it { is_expected.to contain_package('srvadmin-all').with( {:ensure => 'present'} ) }
+  end
 end
 
 
