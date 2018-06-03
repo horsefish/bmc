adf77c5a1cd3d0e178a6b06ec6de1c4679d046ff
MyMarck <nicolai@niro-it.dk>
2018-06-03 23:02:10 +0200

    not working yet

--- spec/unit/facts/manufactor_id_spec.rb
+++ spec/unit/facts/manufactor_id_spec.rb
@@ -8,29 +8,25 @@ describe 'bmc', type: :fact do
     Facter.clear
   end
 
-  describe '#manufactor_id physical' do
-    output = File.join(
-      File.dirname(__FILE__), '..', '..', 'fixtures', 'bmc', 'dell_ipmitool_mc_info.txt'
-    )
-    Facter.fact(:is_virtual).stubs(:value).returns false
-    Facter::Core::Execution.stubs(:which).returns '/bin/ipmitool'
-    Facter::Core::Execution
-      .expects(:execute)
-      .with('/bin/ipmitool mc info 2>&1')
-      .returns(IO.read(output))
-    Facter::Core::Execution
-      .expects(:execute)
-      .with('uname -m', on_fail: nil)
-      .returns 'x86_64'
+  context '#manufactor_id physical' do
+    output = File.join( File.dirname(__FILE__), '..', '..', 'fixtures', 'bmc', 'dell_ipmitool_mc_info.txt' )
+
+    let(:facts) { { :is_virtual => false, } }
+
     it do
-      expect(Facter.fact(:manufactor_id).value).to eq '674'
+      expect(Facter::Core::Execution).to receive(:which).and_return('/bin/ipmitool')
+      expect(Facter::Core::Execution).to receive(:execute).with('/bin/ipmitool mc info 2>&1').and_return(IO.read(output))
+      #allow(Facter::Core::Execution).to receive(:execute).with('uname', '-m').and_return('x86_64')
+
+      expect(Facter.fact(:manufactor_id)).to eq '674'
     end
   end
 
-  describe '#manufactor_id virtual' do
-    Facter.fact(:is_virtual).stubs(:value).returns true
-    it do
-      expect(Facter.fact(:manufactor_id)).to be_nil
-    end
-  end
+#  describe '#manufactor_id virtual' do
+    #Facter.fact(:is_virtual).stubs(:value).returns true
+#    it do
+#      allow(Facter.fact(:is_virtual)).to receive(:value).and_return(true)
+#      expect(Facter.fact(:manufactor_id)).to be_nil
+#    end
+#  end
 end
